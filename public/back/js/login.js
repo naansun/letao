38738110e306ba732aa764871d0be4a23c5c478e
naansun <1783320045@qq.com>
2018-11-19 19:42:39 +0800

    登录功能完成提交

---
+++ public/back/js/login.js
@@ -0,0 +1,89 @@
+//入口函数
+$(function () {
+
+    /* 需求: 表单校验 */
+    /*
+     * 1. 进行表单校验配置
+     *    校验要求:
+     *        (1) 用户名不能为空, 长度为2-6位
+     *        (2) 密码不能为空, 长度为6-12位
+     * */
+    $('#form').bootstrapValidator({
+        //配置小图标
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok', // 校验成功
+            invalid: 'glyphicon glyphicon-remove', // 校验失败
+            validating: 'glyphicon glyphicon-refresh' // 校验中
+        },
+        fields: {
+            //配置用户名
+            username: {
+                //配置校验规则
+                validators: {
+                    //不能为空
+                    notEmpty: {
+                        message: '用户名不能为空'
+                    },
+                    //长度校验
+                    stringLength: {
+                        min: 2,
+                        max: 6,
+                        message: '用户名长度必须在2-6位'
+                    }
+                }
+            },
+            password: {
+                //配置校验规则
+                validators: {
+                    //不能为空
+                    notEmpty: {
+                        message: '密码不能为空'
+                    },
+                    //长度校验
+                    stringLength: {
+                        min: 6,
+                        max: 12,
+                        message: '密码长度必须是6-12位'
+                    }
+                }
+            }
+
+        }
+
+    })
+
+
+
+    //在事件中阻止默认成功的表单提交
+    $("#form").on('success.form.bv', function (e) {
+        e.preventDefault();
+        //使用ajax提交逻辑
+        $.ajax({
+            type: 'post',
+            url: '/employee/employeeLogin',
+            data: $('#form').serialize(),
+            dataType: 'json',
+            success: function (info) {
+                console.log(info);
+                if (info.success) {
+                    location.href = 'index.html';
+                }
+                if (info.error === 1001) {
+                    alert(info.message)
+                }
+                if (info.error === 1000) {
+                    alert(info.message);
+                }
+
+            }
+        })
+    });
+
+
+
+
+    //重置功能
+    $('[type="reset"]').click(function () {
+        $('#form').data("bootstrapValidator").resetForm();
+    })
+})
