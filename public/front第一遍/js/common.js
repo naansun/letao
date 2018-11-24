b4ddeae64e8c0472ae7547d3f1d133802ad8c188
naansun <1783320045@qq.com>
2018-11-24 21:10:01 +0800

    利用swiper实现了轮播图的效果

--- public/front第一遍/js/common.js
+++ public/front第一遍/js/common.js
@@ -3,3 +3,14 @@ mui('.mui-scroll-wrapper').scroll({
     deceleration: 0.0005, //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
     indicators: false,
 });
+var mySwiper = new Swiper('.swiper-container', {
+    direction: 'horizontal', // 垂直切换选项
+    loop: true, // 循环模式选项
+
+    // 如果需要分页器
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    autoplay: true
+
+})
